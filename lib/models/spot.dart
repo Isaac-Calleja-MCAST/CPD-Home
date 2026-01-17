6766c7a1360e08e2dcdc49f05388a86718356310
Isaac-Calleja-MCAST <isaac.calleja.j94845@mcast.edu.mt>
2026-01-17 09:59:50 +0100

    Added dependencies needed for project and created the spot model

---
+++ lib/models/spot.dart
@@ -0,0 +1,15 @@
+class Spot {
+  final String id;
+  final String title;
+  final String imagePath;
+  final double latitude;
+  final double longitude;
+
+  Spot({
+    required this.id,
+    required this.title,
+    required this.imagePath,
+    required this.latitude,
+    required this.longitude,
+  });
+}
